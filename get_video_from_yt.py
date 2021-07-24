e02b886f0b70466e25514a2cf7f3c91679dfc5e2
Crazylov3 <73377942+Crazylov3@users.noreply.github.com>
2021-07-24 23:10:10 +0700

    Add files via upload

--- get_video_from_yt.py
+++ get_video_from_yt.py
@@ -1,6 +1,13 @@
 from youtube_dl import YoutubeDL
 import os
 
+try:
+    if not os.path.exists('input_video'):
+        os.makedirs('input_video')
+
+except OSError:
+    print('Error: Creating directory of data')
+    
 url = "https://www.youtube.com/watch?v=Pb3OEY9XqPA&ab_channel=Ghi%E1%BB%81nQu%E1%BA%A3ngC%C3%A1o"
 with YoutubeDL({'outtmpl': os.path.join("input_video", '%(title)s-%(id)s.%(ext)s')}) as ydl:
     info = ydl.extract_info(url)
